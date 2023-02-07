a38104aa96b0946ceaa5cc9f1bcabb4ba478cfa7
jeremie levan <jeremie.levan@epita.fr>
2023-02-07 16:04:16 +0100

    sprint perso

--- Beginning Is The End/Assets/Scripts/ThirdPersonMovement.cs
+++ Beginning Is The End/Assets/Scripts/ThirdPersonMovement.cs
@@ -11,6 +11,8 @@ public class ThirdPersonMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public float increasespeed = 1f;
+
     // Update is called once per frame
     void Update()
     {
@@ -18,14 +20,26 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+
         if (direction.magnitude >= 0.1f)
         {
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * speed * Time.deltaTime);
+           
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                increasespeed *= 5f;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                increasespeed = 1f;
+            }
+           
+            controller.Move(moveDir * speed * Time.deltaTime * increasespeed);
         }
     }
 }
