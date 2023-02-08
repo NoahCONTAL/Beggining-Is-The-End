71f01fcb8f1642ea40cc7a3a0345d176c164d902
Noah CONTAL <noah.contal@epita.fr>
2023-02-08 23:10:05 +0100

    Mise en place du multi

---
+++ Beginning Is The End/Assets/Scenes/PlayerNetwork.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 56,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+        }
+    }
+
+
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool +"; " + newValue.message);
+        };
+    }
+
+    private void Update() {
+        if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            TestServerRpc();
+            /*
+            randomNumber.Value = new MyCustomData
+            {
+                _int = 10,
+                _bool = false,
+                message = "Tu as tout compris !"
+            };
+            */
+        }
+
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.Z)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.Q)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir* moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRpc()
+    {
+        Debug.Log("TestServerRpc " + OwnerClientId);
+    }
+}
