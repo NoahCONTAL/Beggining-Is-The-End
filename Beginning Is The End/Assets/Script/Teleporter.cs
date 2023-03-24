06cb8480dd906265f2d257fe9bc3c3b52068cef5
Noah Contal <noah.contal@epita.fr>
2023-03-24 10:19:09 +0100

    importation des porte (Tom)

--- Beginning Is The End/Assets/Script/Teleporter.cs
+++ Beginning Is The End/Assets/Script/Teleporter.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     [SerializeField] private Vector3 teleportPosition;
+    [SerializeField] private Vector3 teleportRotation; 
+    
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player") && Input.GetKey(KeyCode.E))
         {
             other.transform.position = teleportPosition;
+            other.transform.rotation = Quaternion.Euler(teleportRotation);
         }
     }
 }
