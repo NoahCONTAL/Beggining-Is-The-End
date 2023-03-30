eda0538625c47d1b0d1468e29dbfd4ad089e3c25
Noah CONTAL <noah.contal@epita.fr>
2023-03-30 12:50:02 +0200

    Ressource folder

--- Beginning Is The End/Assets/Script/Objects/Teleporter.cs
+++ Beginning Is The End/Assets/Script/Objects/Teleporter.cs
@@ -8,16 +8,15 @@ public class Teleporter : NetworkBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (!isServer) return;
-        Debug.Log("passed 1");
-        
+
         if (!other.CompareTag("Player")) return;
-        Debug.Log("passed 2");
 
         var networkManager = NetworkManager.singleton;
 
         networkManager.ServerChangeScene(nextSceneName);
         var spawnPoint = NetworkManager.singleton.GetStartPosition();
-        other.transform.position = spawnPoint.position;
-        other.transform.rotation = spawnPoint.rotation;
+        var playerTransform = other.transform;
+        playerTransform.position = spawnPoint.position;
+        playerTransform.rotation = spawnPoint.rotation;
     }
 }
