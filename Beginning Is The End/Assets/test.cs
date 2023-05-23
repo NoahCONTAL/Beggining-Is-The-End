5b6fa39a1784cd8c2a7e16bb43dd960ae6444d9d
Noah CONTAL <noah.contal@epita.fr>
2023-05-23 10:52:53 +0200

    Update spawned objects on game

--- Beginning Is The End/Assets/test.cs
+++ Beginning Is The End/Assets/test.cs
@@ -1,34 +1,38 @@
-using Mirror;
+using System;
+using System.Collections.Generic;
 using MirrorBasics;
 using UnityEngine;
+using Mirror;
+using Object = UnityEngine.Object;
 
 public class test : NetworkBehaviour
 {
+    [SerializeField] private List<GameObject> objectsToSpawn = new List<GameObject>();
+
+    [SerializeField] private List<Vector3> whereToSpawn = new List<Vector3>();
+
     private string _matchID;
-    private Object[] _objects;
     private Object _o;
 
     private void Start()
     {
         _o = FindObjectOfType(typeof(MirrorBasics.Player));
-        _objects = FindObjectsOfType(typeof(NetworkMatch));
-   
-        MirrorBasics.Player player = _o as MirrorBasics.Player;
-        _matchID = player.matchID;
-        Debug.Log($"Match ID: {_matchID}");
 
-        if (isServer)
+        MirrorBasics.Player player = _o as MirrorBasics.Player;
+        if (player != null)
         {
-            if (_objects is GameObject[] gameObjects)
-            {
-                foreach (GameObject o in gameObjects)
-                {
-                    o.GetComponent<NetworkMatch>().matchId = _matchID.ToGuid();
-                }
-            }
+            _matchID = player.matchID;
+            Debug.Log($"Match ID: {_matchID}");
         }
-        
-        Instantiate(this.gameObject);
-        NetworkServer.Spawn(this.gameObject);
+        for (int i = 0; i < objectsToSpawn.Count; i++)
+        { 
+            var temp = Instantiate(objectsToSpawn[i]);
+            NetworkServer.Spawn(temp);
+            
+            temp.GetComponent<NetworkMatch>().matchId = _matchID.ToGuid();
+            temp.transform.position = whereToSpawn[i];
+
+            Debug.Log($"{temp.name} spawned at {temp.transform.position}");
+        }
     }
 }
